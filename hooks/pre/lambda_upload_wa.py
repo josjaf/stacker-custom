bc6ff95134fbd12153ad5986bd5c9f688362c630
Josh Jaffe <josjaf@amazon.com>
2020-12-09 14:19:41 -0500

    updating upload hook

--- hooks/pre/lambda_upload_wa.py
+++ hooks/pre/lambda_upload_wa.py
@@ -1,7 +1,7 @@
 from stacker.hooks.aws_lambda import upload_lambda_functions
 
 
-def custom_upload_lambda_functions(context, provider, **kwargs):
+def custom_lambda_upload(context, provider, **kwargs):
     new_results = {}
     results = upload_lambda_functions(context, provider, **kwargs)
     for name, code_obj in results.items():
@@ -10,3 +10,21 @@ def custom_upload_lambda_functions(context, provider, **kwargs):
             "s3_bucket": code_obj.S3Bucket
         }
     return new_results
+
+
+"""
+pre_build:
+  - path: hooks.pre.lambda_upload_wa.custom_upload_lambda_functions #lambda_upload_wa.custom_upload_lambda_functions
+    required: true
+    enabled: true
+    data_key: alert_lambda
+    args:
+      bucket: ${stacker_bucket}
+      follow_symlinks: false
+      prefix: predict_lambda/
+      functions:
+        predict_lambda:
+          path: lambda_function/alert_lambda/
+          include:
+            - "*.py"
+"""
